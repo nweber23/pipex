9d7d30de607acf4a88a2af5a10eef6500227102d
Niklas Weber <nweber@student.42heilbronn.de>
2025-07-31 15:08:55 +0200

    init

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME = pipex
+SRC =
+OBJ_DIR = objs
+OBJ = $(SRC:srcs/%.c=$(OBJ_DIR)/%.o)
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -I./includes/ -I./libft/includes/
+
+# Library paths
+LIBFT_DIR = ./libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+all: $(LIBFT) $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+
+$(OBJ_DIR)/%.o: srcs/%.c includes/*.h
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR)
+	$(MAKE) clean -C $(LIBFT_DIR)
+
+fclean: clean
+	rm -f $(NAME)
+	$(MAKE) fclean -C $(LIBFT_DIR)
+
+re:
+	$(MAKE) fclean
+	$(MAKE) all
+
+.PHONY: all clean fclean re
---
+++ includes/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:00:21 by nweber            #+#    #+#             */
+/*   Updated: 2025/07/31 15:05:56 by nweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <sys/wait.h>
+
+#endif
