760ff04164b91939545642a1766d5a3335b4731f
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-01 14:53:28 +0200

    more tests for understanding

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 15:17:01 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/01 13:40:32 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/01 14:50:11 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,32 @@ void	exit_handling(void)
 	exit(0);
 }
 
-int	main(int argc, char **argv)
+void	parent_prcs(int *fd, char **argv, char **envp)
 {
-	int	fd[2];
+	int	out_file;
+
+	out_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+}
+
+void	child_prcs(int *fd, char **argv, char **envp)
+{
+	int	in_file;
+
+	in_file = open(argv[1], O_RDONLY, 0777);
+}
+
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	pid_t	pid;
 
 	if (argc != 5)
 		exit_handling();
+	if (pipe(fd) == -1)
+		exit(-1);
+	pid = fork();
 	// open file1
 	// open or create file 2
 	// create pipe
