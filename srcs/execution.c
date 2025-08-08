ebb9195f1ae297021d2ee5ef35eb6975bb5ba166
Niklas Weber <nweber@student.42heilbronn.de>
2025-08-08 15:04:42 +0200

    protected strdup for malloc failure

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 11:06:23 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/03 13:06:10 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/08 14:50:15 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,23 @@ static char	**prepare_shell_command(char *cmd)
 	if (!all_cmds)
 		exit(EXIT_FAILURE);
 	all_cmds[0] = ft_strdup("sh");
+	if (!all_cmds[0])
+	{
+		ft_array_free(all_cmds);
+		exit(EXIT_FAILURE);
+	}
 	all_cmds[1] = ft_strdup("-c");
+	if (!all_cmds[1])
+	{
+		ft_array_free(all_cmds);
+		exit(EXIT_FAILURE);
+	}
 	all_cmds[2] = ft_strdup(cmd);
+	if (!all_cmds[2])
+	{
+		ft_array_free(all_cmds);
+		exit(EXIT_FAILURE);
+	}
 	all_cmds[3] = NULL;
 	return (all_cmds);
 }
